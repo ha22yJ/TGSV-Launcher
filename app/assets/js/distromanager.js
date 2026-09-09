c0d052e8471baf4e0c0e74bfa3adf2d937d0837e
ha22yJ <327182172+ha22yJ@users.noreply.github.com>
2026-09-10 07:13:59 +0900

    Prepare TGSV Launcher 1.0.0 release

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -4,7 +4,7 @@ const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = 'https://helios-files.geekcorner.eu.org/distribution.json'
+exports.REMOTE_DISTRO_URL = 'https://pub-fb3e91d5a33249dc9a4bae2fa98f157d.r2.dev/distribution_dev.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
